5a4e50e7b1810b05edb7e478df6d10bb5bfcc62a
Pia-Fierro <piafierro.3187@gmail.com>
2023-04-27 16:56:32 +1000

    cleaned code at savedBooks.js

--- Develop/client/src/pages/SavedBooks.js
+++ Develop/client/src/pages/SavedBooks.js
@@ -28,13 +28,6 @@ const SavedBooks = () => {
       // const response =
       await removeBook({ variables: { bookId } });
 
-      // if (!response.ok) {
-      //   throw new Error("something went wrong!");
-      // }
-
-      // const updatedUser = await response.json();
-      // setUserData(updatedUser);
-
       // upon success, remove book's id from localStorage
       removeBookId(bookId);
     } catch (err) {
